c43d1d381dc83779ceef0a462e9ef3da3bbaf9d1
sougannkyou <songyq0509@163.com>
2018-08-05 22:39:53 +0800

    @home

--- 5.py
+++ 5.py
@@ -1,19 +1,32 @@
 # coding:utf8
 import sys
-import cv2
 import numpy as np
+from  PIL import Image
+import cv2
 
 
 def GetRowRects(img):
     rows = []
     obj = cv2.imread(img)
     h, w, c = obj.shape
-    projection =
+    # 用于存储投影值
+    projection = [0 for _ in range(h)]
 
-    cv2.getRotationMatrix2D()
+    # 遍历每一行计算投影值
+    for y in range(h):
+        for x in range(w):
+            im = Image.open('image.gif')
+            rgb_im = im.convert('RGB')
+            r, g, b = rgb_im.getpixel((1, 1))
+        {
+            var
+        s = Cv.Get2D(source, y, x);
+        if (s.Val0 == 255)
+        projection[y]++;
 
 
-img = cv2.imread('shapes.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if __name__ == '__main__':
+    img = cv2.imread('shapes.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+    ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
