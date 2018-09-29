2a1721327cae15ea10e77e80df9f50f8ba826693
sougannkyou <songyq0509@163.com>
2018-09-29 19:36:26 +0800

    @company

--- car_baidu/download_img.py
+++ car_baidu/download_img.py
@@ -1,56 +1,17 @@
 import os
+import json
 import redis
 import urllib.request
 from datetime import datetime
 from pprint import pprint
-from aip import AipImageClassify
 
-APP_ID = '14131380'
-API_KEY = 'KyCbH0iKqkr1usuRAQR5CSga'
-SECRET_KEY = 'IkS88smDf5Ab8OGvobIsDzM2x9zWRB7g'
-
-REDIS_SERVER = 'redis://192.168.174.130/11'
+REDIS_SERVER = 'redis://192.168.174.130/10'
 img_key = 'weibo_pic_l'
-images_path = 'images\\'
-conn = redis.StrictRedis.from_url(REDIS_SERVER)
+img_detection_key = 'weibo_pic_detector_h'
+conn = redis.Redis(host='192.168.174.130', port='6379', db=10, decode_responses=True)
+local_conn = redis.Redis(host='172.16.253.37', port='6379', db=10, decode_responses=True)
 
-
-def get_file_content(file_path):
-    with open(file_path, 'rb') as fp:
-        return fp.read()
-
-
-def detection(file_path):
-    client = AipImageClassify(APP_ID, API_KEY, SECRET_KEY)
-    image = get_file_content(file_path)
-    client.carDetect(image)
-    ret = client.carDetect(image, {'top_num': 10, 'baike_num': 5})  # 带参数调用车辆识别
-    pprint(ret)
-    return ret
-
-
-def uploadAPI(request):
-    ret = {'img_src': 'error.png'}
-    try:
-        img_src = str(int(datetime.now().timestamp() * 1000000))
-        file_obj = request.FILES.get('file')
-        fixed = file_obj.name[file_obj.name.find('.'):]
-        img_path = os.path.join(STATIC_ROOT, 'upload', img_src + fixed)
-        ret = {'img_src': '/static/upload/{}{}'.format(img_src, fixed)}
-        print(img_path)
-        f = open(img_path, 'wb')
-        # print(file_obj, type(file_obj))
-        for chunk in file_obj.chunks():
-            f.write(chunk)
-        f.close()
-        ret.update(detection(img_path))
-    except Exception as e:
-        print('error:', e)
-    finally:
-        print('ok')
-
-
-def dowmload():
+def download():
     # {
     #     "data_db": "redis:\/\/redis-collectioncache-1.istarshine.net.cn\/3\/sina_data",
     #     "siteName": "\u65b0\u6d6a\u5fae\u535a",
@@ -67,9 +28,26 @@ def dowmload():
     #     "replyCount": 0,
     #     "pic_urls": [
     #         "http:\/\/wx4.sinaimg.cn\/thumbnail\/0067l5Kxly1fvo76gxd10j30qo0zkgsu.jpg"
-    #     ]
+    #     ],
+    #     "detect":[{},{},{}]
     # }
-    img_info = conn.lpop(img_key)
-    conn.lpush(img_key, img_info)
-    for url in img_info['pic_urls']:
-        urllib.request.urlretrieve(url, images_path)
+    for _ in range(min(conn.llen(img_key), 10)):
+        v = conn.lpop(img_key)
+        conn.rpush(img_key, v)
+        local_conn.rpush(img_key, v)  # backup
+        img_info = json.loads(v)
+        if not conn.hexists(img_detection_key, img_info['mid']):
+            img_info.update({"detect": [{} for _ in range(len(img_info['pic_urls']))]})
+            conn.hset(img_detection_key, img_info['mid'], img_info)
+            # pprint(img_info)
+            i = 0
+            for url in img_info['pic_urls']:
+                urllib.request.urlretrieve(
+                    url.replace('thumbnail', 'large'),
+                    os.path.join(os.getcwd(), 'images', '{}_{}.jpg'.format(img_info['mid'], i))
+                )
+                i += 1
+
+
+if __name__ == '__main__':
+    download()
